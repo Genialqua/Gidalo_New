f3869a708aa904ea9cf397e344d787849c6622c4
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-07-24 16:52:04 +0100

    The edited mvp of Gidalo production ready

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+if (process.env.NODE_ENV !== 'production') {
+    // Set static folder // Serve static files from the client/build folder
+    app.use(express.static(path.join(__dirname, '/frontend/build'))); 
+    
+    app.get('*', (req,res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+);
+} else {
+    app.get('/', (req, res) =>{
+        res.send('API is running.....');  // Send a message when the server is running in production mode
+    })
+}
+
+
 app.get('/', (req, res) => {
     res.send('API is running'); 
 });
