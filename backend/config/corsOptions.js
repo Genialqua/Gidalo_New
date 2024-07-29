1ef2b47e0d192cbd86ff0c55aa8384b5d1b8e9b9
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-07-29 09:05:12 +0100

    Reinstalled cors

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,19 +1,14 @@
 import allowedOrigins from './allowedOrigins.js';
-import cors from 'cors';
 
 const corsOptions = {
-
-    origin: '*', // Allow all origins
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     optionsSuccessStatus: 200,
-
-    // origin: (origin, callback) => {
-    //     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-    //         callback(null, true);
-    //     } else {
-    //         callback(new Error('Not allowed by CORS'));
-    //     }
-    // },
-    // optionsSuccessStatus: 200,
 };
 
 export default corsOptions;
