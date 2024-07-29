428e35dfb5e15b21cc9352410b968684f133cd07
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-07-29 08:20:21 +0100

    Debugged cors to allow all origins

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,14 +1,19 @@
 import allowedOrigins from './allowedOrigins.js';
+import cors from 'cors';
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+
+    origin: '*', // Allow all origins
     optionsSuccessStatus: 200,
+
+    // origin: (origin, callback) => {
+    //     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    //         callback(null, true);
+    //     } else {
+    //         callback(new Error('Not allowed by CORS'));
+    //     }
+    // },
+    // optionsSuccessStatus: 200,
 };
 
 export default corsOptions;
