1087a5c888c7b881d7e7d472b8702507d0e0a849
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-07-29 08:09:01 +0100

    Debugged cors conditions

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     "http://localhost:3000",
+    " https://gidalo-new-frontend.vercel.app",
     "https://gidalo-new-frontend-1m723rls5.vercel.app",
     "https://www.gidalo.com",
   "https://gidalo.com"
