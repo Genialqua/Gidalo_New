e4598fb6b9d97f6111359e8f481cd67b0bb265d0
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-07-30 10:14:49 +0100

    Editted cors file new

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,9 +1,7 @@
 const allowedOrigins = [
     "http://localhost:3000",
-    "https://gidalo-new-frontend-8x1qndmoj.vercel.app",
-    "https://gidalo-new-frontend-6w47xhtpa.vercel.app",
-    "https://www.gidalo.com",
-  "https://gidalo.com"
+    "https://gidalo-new-frontend.vercel.app "
+
   ];
 
   export default allowedOrigins;
