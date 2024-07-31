312fe8ef9aef74ac0bdf9faea5dc6424c91a57e3
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-07-31 01:22:28 +0100

    Made changes to Origin statements again

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,3 +1,5 @@
+
+
 const allowedOrigins = [
     "http://localhost:3000",
     "https://gidalo-new-frontend.vercel.app:3000",
--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,3 +1,4 @@
+
 import allowedOrigins from './allowedOrigins.js';
 
 const corsOptions = {
