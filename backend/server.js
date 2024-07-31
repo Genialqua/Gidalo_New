86e592acb58a7bbdc91b671090afa87c77bda902
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-07-31 13:21:24 +0100

    Editted the server file again

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === 'production') {
   // Serve static files from the client/build folder
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+//   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
