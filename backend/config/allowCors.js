314b25afcab750827778db75172daf76afddadc2
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-08-01 09:15:00 +0100

    Re-configured the CORS file and server file 1

--- backend/config/allowCors.js
+++ backend/config/allowCors.js
@@ -1,3 +1,5 @@
+import { response } from "express";
+
 const allowCors = (req, res) => {
     const allowedOrigins = [
       'https://gidalo-new-frontend.vercel.app',
@@ -10,9 +12,9 @@ const allowCors = (req, res) => {
       res.setHeader('Access-Control-Allow-Origin', origin);
     }
     
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, Accept-Version, X-Requested-With, X-CSRF-Token, Content-Length, Content-MD5, Date, X-Api-Version');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    response.setHeader('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']);
+    response.setHeader('Access-Control-Allow-Headers', ['Content-Type', 'Accept', 'Accept-Version', 'X-Requested-With', 'X-CSRF-Token', 'Content-Length', 'Content-MD5', 'Date', 'X-Api-Version']);
+    response.setHeader('Access-Control-Allow-Credentials', 'true');
   
     if (req.method === 'OPTIONS') {
       res.status(200).end();
