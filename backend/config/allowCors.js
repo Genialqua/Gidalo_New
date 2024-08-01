a2bf4a9b4a71feb80e4312f53934895ac54a3e95
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-08-01 09:55:59 +0100

    Re-configured the CORS file and server file 2

--- backend/config/allowCors.js
+++ backend/config/allowCors.js
@@ -1,28 +1,58 @@
-import { response } from "express";
-
-const allowCors = (req, res) => {
+const allowCors = (req, res, next) => {
     const allowedOrigins = [
-      'https://gidalo-new-frontend.vercel.app',
-      'http://localhost:3000'
+        'https://gidalo-new-frontend.vercel.app',
+        'http://localhost:3000'
     ];
     
     const origin = req.headers.origin;
     
     if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
+        res.setHeader('Access-Control-Allow-Origin', origin);
     }
     
-    response.setHeader('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']);
-    response.setHeader('Access-Control-Allow-Headers', ['Content-Type', 'Accept', 'Accept-Version', 'X-Requested-With', 'X-CSRF-Token', 'Content-Length', 'Content-MD5', 'Date', 'X-Api-Version']);
-    response.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, Accept-Version, X-Requested-With, X-CSRF-Token, Content-Length, Content-MD5, Date, X-Api-Version');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
   
     if (req.method === 'OPTIONS') {
-      res.status(200).end();
+        res.status(200).end();
     } else {
         next();
     }
-
-  };
+};
   
-  export default allowCors;
+export default allowCors;
+
+
+
+
+
+
+// import { response } from "express";
+
+// const allowCors = (req, response) => {
+//     const allowedOrigins = [
+//       'https://gidalo-new-frontend.vercel.app',
+//       'http://localhost:3000'
+//     ];
+    
+//     const origin = req.headers.origin;
+    
+//     if (allowedOrigins.includes(origin)) {
+//       res.setHeader('Access-Control-Allow-Origin', origin);
+//     }
+    
+//     response.setHeader('Access-Control-Allow-Methods', ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']);
+//     response.setHeader('Access-Control-Allow-Headers', ['Content-Type', 'Accept', 'Accept-Version', 'X-Requested-With', 'X-CSRF-Token', 'Content-Length', 'Content-MD5', 'Date', 'X-Api-Version']);
+//     response.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+//     if (req.method === 'OPTIONS') {
+//       res.status(200).end();
+//     } else {
+//         next();
+//     }
+
+//   };
+  
+//   export default allowCors;
   
