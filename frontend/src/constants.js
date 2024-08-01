4bc196697b98e6829aa8f841d1dd3b0cb8a2c531
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-08-01 10:33:56 +0100

    Re-configured the CORS file and server file 4

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -2,7 +2,7 @@
 //   ? 'https://gidalo-new-backend-cs1q7b0lp-frederick-aleoke-malachis-projects.vercel.app'
 // : 'https://localhost:5001' ; // Define production URL
  
-export const BASE_URL = 'https://gidalo-new-backend-7kjtulv2y-frederick-aleoke-malachis-projects.vercel.app';
+export const BASE_URL = 'https://gidalo-new-backend.vercel.app';
 //export const BASE_URL = '';
 export const PROPERTIES_URL = '/api/properties';
 export const USERS_URL = '/api/users';
