223970ee23713b25a8a59ef6e50c8e06e15349e5
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-09-08 17:57:50 +0100

    Updated cors to accept authorization

--- backend/config/allowCors.js
+++ backend/config/allowCors.js
@@ -4,7 +4,8 @@ const allowCors = fn => async (req, res, next) => {
         'https://gidalo.com',
         'https://gidalo-new-frontend.vercel.app',
         'http://localhost:3000',
-        'localhost/:1'
+        'localhost/:1',
+        'localhost:3000'
     ];
     
     const origin = req.headers.origin;
@@ -15,7 +16,7 @@ const allowCors = fn => async (req, res, next) => {
     }
     
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, Accept-Version, X-Requested-With, X-CSRF-Token, Content-Length, Content-MD5, Date, X-Api-Version');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, Accept-Version, X-Requested-With, X-CSRF-Token, Content-Length, Content-MD5, Date, X-Api-Version');
     //res.setHeader('Access-Control-Allow-Credentials', 'true');
   
     if (req.method === 'OPTIONS') {
